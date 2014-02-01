45148aea2e6c96479076536ad9bac601f971a208
alexpotter1 <alexanderpotter2010@gmail.com>
2014-02-01 14:57:23 +0000

    adding equation program

---
+++ prog1.py
@@ -0,0 +1,15 @@
+import math
+
+class Program:
+    equation = input("Please type an equation to check: ")
+    result = str(eval(equation))
+    
+   
+
+
+   
+print("Your result is: "+Program.result)
+
+
+
+
