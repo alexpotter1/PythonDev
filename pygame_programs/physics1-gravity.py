7641c57d1c31a5bba469ade5ce428f201b167ea6
alexpotter1 <alexanderpotter2010@gmail.com>
2014-03-28 21:50:57 +0000

    Refactoring code

--- pygame_programs/physics1-gravity.py
+++ pygame_programs/physics1-gravity.py
@@ -6,16 +6,62 @@ try:
 except ImportError:
     print("Please install Pygame! ('sudo apt-get install python-pygame')")
 
+class Ball():
+	position = [0,0]
+	colour = (255,255,255)
+	speed = [2,2] # speed that the ball will move (x,y)
+	width = 640
+	height = 480 # symlink to size in main program 
+	
+	
+	
+	def __init__(self):
+		print('inited')
+		
+	def setColour(self, (r, g, b)):
+		self.colour = (r, g, b)
+		
+	def getColour(self):
+		return self.colour
+		
+	def createBall(self, sizex, sizey, rad):
+		self.sizex = sizex
+		self.sizey = sizey
+		self.rad = rad
+		
+		ball = pygame.Surface((self.sizex, self.sizey)) # creates new ball surface
+		self.ball = ball
+		
+		pygame.draw.circle(self.ball,self.getColour(),[25,25], self.rad) # x,y,radius
+		ballrectangle = self.ball.get_rect(center=([self.sizex/2, self.sizey/2])) # attaches collision box to ball
+		self.ballrectangle = ballrectangle	
+		
+	def Physics(self):
+		self.ballrectangle = self.ballrectangle.move(self.speed)
+		
+		if (self.ballrectangle.left < 0 or self.ballrectangle.right > self.width):
+			#speed[0] *= momentum # reduces ball energy when colliding with edge of window
+			self.speed[0] = -self.speed[0]
+		if (self.ballrectangle.top < 0 or self.ballrectangle.bottom > self.height):
+			#speed[1] *= momentum
+			self.speed[1] = -self.speed[1]
+		
+	
 
+		
 
+	
+		
+		
 pygame.init()
 size = width, height = 640,480
+
 black = 0,0,0
 white = 255,255,255
+azure = 0,127,255
 red = 255,0,0
 
-gravity = 0.8
-momentum = 0.8
+
 
 pygame.display.set_caption("Alex's Game")
 screen = pygame.display.set_mode(size)
@@ -26,11 +72,6 @@ paddle.fill(white)
 paddlerectangle = paddle.get_rect(center=((size[0]/2,size[1]-50)))# places paddle rectangle to half of the screen width
 
 
-
-ball = pygame.Surface((50,50)) # creates new ball surface
-ballrectangle = ball.get_rect(center=(50,100)) # attaches collision box to ball
-speed = [2,2] # speed that the ball will move (x,y)
-pygame.draw.circle(ball,red,[25,25], 25) # x,y,radius
 
 # init new clock
 clock = pygame.time.Clock()
@@ -56,18 +97,25 @@ def FPS(targetFPS):
 def draw():
     FPS(60)
     screen.fill(black)
-    screen.blit(ball,ballrectangle) # moves ball onto screen
+    screen.blit(b1.ball,b1.ballrectangle) # moves ball onto screen
+    screen.blit(b2.ball,b2.ballrectangle)
     screen.blit(FPS.fpsCounter, FPS.fpsCounterRect)
     screen.blit(paddle, paddlerectangle)
     pygame.display.flip() # double buffering, waits until frame is fully drawn
 
 
 
+b1 = Ball()
+b1.setColour(azure)
+b1.createBall(50,50,25)
+
+b2 = Ball()
+b2.setColour(red)
+b2.createBall(50,50,25)
 
 
 MouseLeftButton = 0
 step = 2
-BallBottom = 0
 running = True
 while running:
     for event in pygame.event.get():
@@ -83,40 +131,30 @@ while running:
     position = pygame.mouse.get_pos()
     x = int(position[0])
     paddlerectangle.right = x
+    
+    if paddlerectangle.colliderect(b1.ballrectangle):
+		#speed[1] *= momentum
+		b1.speed[1] = -b1.speed[1]
+
+    #speed[1] += gravity
 
 
-    speed[1] += gravity
+    b1.Physics()
+		
 
 
-			
-    ballrectangle = ballrectangle.move(speed)
-    if (ballrectangle.left < 0 or ballrectangle.right > width):
-        speed[0] *= momentum # reduces ball energy when colliding with edge of window
-        speed[0] = -speed[0]
-    if (ballrectangle.top < 0 or ballrectangle.bottom > height):
-        speed[1] *= momentum
-        speed[1] = -speed[1]
 
-
-    if ballrectangle.bottom > height:
-        BallBottom = 1
-
-    if BallBottom == 1:
+    if b1.ballrectangle.bottom > height:
         screen.blit(text, textRect)
         pygame.display.update()
+        pygame.time.delay(2000)
+        b1.ballrectangle.top = 0
 
 
-
-	
-	if paddlerectangle.colliderect(ballrectangle):
-		speed[1] *= momentum
-        speed[1] = -speed[1]
-
-
-    ballrectangle.left = clip(ballrectangle.left, 0, width)
-    ballrectangle.right = clip(ballrectangle.right, 0, width)
-    ballrectangle.top = clip(ballrectangle.top, 0, height)
-    ballrectangle.bottom = clip(ballrectangle.bottom, 0, height)
+    b1.ballrectangle.left = clip(b1.ballrectangle.left, 0, width)
+    b1.ballrectangle.right = clip(b1.ballrectangle.right, 0, width)
+    b1.ballrectangle.top = clip(b1.ballrectangle.top, 0, height)
+    b1.ballrectangle.bottom = clip(b1.ballrectangle.bottom, 0, height)
     
     paddlerectangle.left = clip(paddlerectangle.left, 0, width)
     paddlerectangle.right = clip(paddlerectangle.right, 0,width)
